68e35d817c293ca97c465a6f98683bd40f31d762
Jon Daniel <binarycleric@gmail.com>
2011-09-03 01:51:34 -0400

    You'd think I could write proper bash by now.

--- profile-base/.profile.d/development-macros.sh
+++ profile-base/.profile.d/development-macros.sh
@@ -1,6 +1,12 @@
 # Moves into the specified codebase and opens that 
 # directory using your specified editor.
 code() {
-  cd "$HOME/src/$@/" 2> /dev/null || echo "Codebase $@ not found in $HOME/src"; return 1
+  cd "$HOME/src/$@/" 2> /dev/null
+  if [ "$?" != "0" ]
+  then
+    echo "Codebase $@ not found in $HOME/src"
+    return 1
+  fi
+
   $EDITOR .
 }
