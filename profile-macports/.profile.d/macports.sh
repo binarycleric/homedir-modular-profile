d3960676c033a59a445b973c46998c75cbedb904
Jon Daniel <binarycleric@gmail.com>
2011-09-02 22:16:11 -0400

    Moved macports stuff into it's own package.

--- profile-macports/.profile.d/macports.sh
+++ profile-macports/.profile.d/macports.sh
@@ -4,7 +4,7 @@
 export MANPATH=/opt/local/share/man:$MANPATH
 export DISPLAY=:0.0
 
-# MacPorts Installer addition on 2009-08-29_at_01:28:42: adding an appropriate 
-# PATH variable for use with MacPorts.
+# MacPorts Installer addition: adding an appropriate PATH 
+# variable for use with MacPorts.
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 
